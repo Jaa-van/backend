91c40052720c630e2b11a28b67d088df4dac5a43
SUMMER-DESKTOP\jungh <junghs710@naver.com>
2023-05-08 17:47:35 +0900

    로그인/회원가입

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api', router);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
---
+++ servies/users.service.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const UserRepository = require("../repositories/users.repository");
+
+class UserService {
+  userRepository = new UserRepository();
+
+  existUsers = async (nickname) => {
+    const existUsers = await this.userRepository.existUsers(nickname);
+    return existUsers;
+  };
+
+  signup = async (email, nickname, password) => {
+    const signup = await this.userRepository.signup(email, nickname, password);
+    return signup;
+  };
+
+  login = async (email) => {
+    const login = await this.userRepository.login(email);
+    return login;
+  };
+
+  tokenmake = async (login) => {
+    const token = jwt.sign({ userId: login.userId }, "clo_key");
+    return token;
+  };
+}
+
+module.exports = UserService;
