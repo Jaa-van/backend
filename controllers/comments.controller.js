44bcef0bca3b713ee0ed666cfc0df38d3358d7e6
namu56 <107787137+namu56@users.noreply.github.com>
2023-05-08 20:49:07 +0900

    #10 댓글 수정 기능 구현 (#11)
    * 댓글 수정 기능 구현

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -26,7 +26,7 @@ class CommentsController {
       throw new Error("400/댓글 작성에 실패하였습니다.");
     }
   };
-  
+
   // 댓글 조회
 
   getComments = async (req, res, next) => {
@@ -43,6 +43,39 @@ class CommentsController {
     } catch (error) {
       console.error(error);
       throw new Error("400/댓글 조회에 실패하였습니다.");
+    }
+  };
+
+  // 댓글 수정
+
+  updateComment = async (req, res, next) => {
+    const { userId } = res.locals.user;
+    const { postId, commentId } = req.params;
+    const { comment } = req.body;
+    const post = await this.commentService.findOnePost(postId);
+    const commentOfPost = await this.commentService.findOneComment(commentId);
+
+    try {
+      if (!post) {
+        throw new Error("404/게시글이 존재하지 않습니다.");
+      }
+
+      if (userId !== post.UserId) {
+        throw new Error("403/댓글의 수정 권한이 존재하지 않습니다.");
+      }
+      if (!commentOfPost) {
+        throw new Error("404/댓글이 존재하지 않습니다.");
+      }
+
+      if (!comment) {
+        throw new Error("412/데이터 형식이 올바르지 않습니다.");
+      }
+
+      await this.commentService.updateComment(comment, commentId);
+      return res.status(200).json({ message: "댓글을 수정하였습니다." });
+    } catch (error) {
+      console.error(error);
+      throw new Error("404/댓글이 존재하지 않습니다.");
     }
   };
 }
