2c23d1d883b1d16cc9a02f5278dd6273b0f4a9be
namu56 <swi920224@gmail.com>
2023-05-08 17:32:30 +0900

    comment 생성

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,6 +2,32 @@ const CommentsService = require("../servies/comments.service");
 
 class CommentsController {
   commentsService = new CommentsService();
+
+  // 댓글 생성
+  createComment = async (req, res, next) => {
+    const { userId } = res.locals.user;
+    const { postId } = req.params;
+    const { comment } = req.body;
+    const post = await this.commentsService.findOnePost(postId);
+
+    if (!post) {
+      throw new Error("404/게시글이 존재하지 않습니다.");
+    }
+
+    if (!comment) {
+      throw new Error("400/댓글 내용을 입력해주세요.");
+    }
+
+    try {
+      await this.commentsService.createComment(userId, postId, comment);
+      return res.status(200).json({ message: "댓글을 생성하였습니다." });
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(404)
+        .json({ errorMessage: "댓글 작성에 실패하였습니다." });
+    }
+  };
 }
 
 module.exports = CommentsController;
--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -1,4 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+// 댓글 생성 API
+router.post(
+  "/:postId/comments",
+  authMiddleware,
+  commentsController.createComment,
+);
+
+// 댓글 목록 조회 API
+router.get("/:postId/comments", commentsController.getComments);
+
+// 댓글 수정 API
+router.put(
+  "/:postId/comments/:commentId",
+  authMiddleware,
+  commentsController.updateComment,
+);
+
+// 댓글 삭제 API
+router.delete(
+  "/:postId/comments/:commentId",
+  authMiddleware,
+  commentsController.deleteComment,
+);
 module.exports = router;
