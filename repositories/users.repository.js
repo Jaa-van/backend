913aee579cda64286dbd167f0907c4920fac0c6c
Jaa-van <127918643+Jaa-van@users.noreply.github.com>
2023-05-09 20:18:31 +0900

    signup errorhandling update commit

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -1,21 +1,27 @@
 const { Users } = require("../models");
 
 class UserRepository {
+  existUsers = async (nickname) => {
+    const existUsers = await Users.findOne({ where: { nickname } });
+    return existUsers;
+  };
 
-    existUsers = async ( nickname ) => {
-        const existUsers = await Users.findOne( {where : {nickname}} );
-    }
+  existEmail = async (email) => {
+    const existEmail = await Users.findOne({
+      where: { email },
+    });
+    return existEmail;
+  };
 
-    signup = async ( email, nickname, password ) => { 
-        const signup = await Users.create({ email, nickname, password }); 
-        return signup;
-    };
+  signup = async (email, nickname, password) => {
+    const signup = await Users.create({ email, nickname, password });
+    return signup;
+  };
 
-    login = async ( email ) => {
-        const login = await Users.findOne({ where: { email } });
-        return login;
-    };
-
-};
+  login = async (email) => {
+    const login = await Users.findOne({ where: { email } });
+    return login;
+  };
+}
 
 module.exports = UserRepository;
